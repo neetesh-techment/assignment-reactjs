f30a20a848b7ce96ff834967eb4b5fd65ffb9696
Neetesh Gupta <neetesh.g@techment.com>
2020-04-30 12:52:31 +0530

    disable input button if input is blank

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_KEY = 'XQwNWMtBfoKitMkWCbBh2wDdM0V6nYCudfkMQ2qU'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bool: true,
+      asteroidInputVal: '',
+      name: '',
+      nasaJplUrl: '',
+      isPotentiallyHazardousAsteroid: ''
+    }
+  }
+
+  changeValue(evt){
+    this.setState({asteroidInputVal: evt.target.value})
+
+    if(evt.target.value.length > 0){
+      this.setState({bool: false})
+    }else{
+      this.setState({bool: true})
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type='text'
+               value={this.state.asteroidInputVal}
+               placeholder="Enter Asteroid ID"
+               onChange={inputValue => this.changeValue(inputValue)} />
+        <button disabled={this.state.bool}>Submit</button>
+        <button >Random Asteroid</button>
+
+        <div>
+          <p>Name: {this.state.name}</p>
+          <p>Nasa Jpl URL: {this.state.nasaJplUrl}</p>
+          <p>Is Potentially Hazardous Asteroid: {this.state.isPotentiallyHazardousAsteroid}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
